a5ea2cb46a75d16341d99641b34cecfca27ad403
Athos Francisco Pereira do Carmo <athos.carmo@mcmec.mcmmontagens.com.br>
2018-11-12 10:19:01 -0300

    até a aula 27

--- app/Http/Controllers/Painel/ProdutoController.php
+++ app/Http/Controllers/Painel/ProdutoController.php
@@ -5,20 +5,22 @@ namespace App\Http\Controllers\Painel;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\Painel\Product;
+use App\Http\Requests\Painel\ProductFormRequest;
 
-class ProdutoController extends Controller
-{
+class ProdutoController extends Controller {
+
     private $product;
-    
+
     public function __construct(Product $product) {
         $this->product = $product;
     }
-    
-    public function index(Product $product)
-    {
-        $products = $this->product->all();
+
+    public function index() {
+        $title = 'Listagem dos Produtos';
         
-        return view('painel.products.index', compact('products'));
+        $products = $this->product->all();
+
+        return view('painel.products.index', compact('products', 'title'));
     }
 
     /**
@@ -26,9 +28,10 @@ class ProdutoController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function create()
-    {
-        //
+    public function create() {
+        $title = 'Cadastrar Novo Produto';
+        $categorys = ['eletronicos', 'moveis', 'limpeza', 'banho'];
+        return view('painel.products.create-edit', compact('title', 'categorys'));
     }
 
     /**
@@ -37,9 +40,42 @@ class ProdutoController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
-    {
-        //
+    public function store(ProductFormRequest $request) {
+        
+        //dd($request->all());
+        //dd($request->only(['name', 'number']));
+        //dd($request->except(['_token', 'category']));
+        //dd($request->input('name'));
+        
+        $dataForm = $request->all();
+        
+        $dataForm['active'] = (!isset($dataForm['active'])) ? 0 : 1;
+        
+        //Validação
+        //$this->validate($request, $this->product->rules);
+        /*$messages = [
+            'name.required' => 'O campo nome é de preenchimento obrigatório!',
+            'number.numeric' => 'Precisa ser apenas números!',
+            'number.required' => 'O campo número é de preenchimento obrigatório!'
+        ];
+         * 
+         *
+        
+        $validate = validator($dataForm, $this->product->rules);
+        if($validate->fails()){
+            return redirect()->route('produtos.create')->withErrors($validade)->withInput();
+        }
+         * 
+         */
+        
+        //Faz o cadastro
+        $insert = $this->product->create($dataForm);
+        
+        if($insert){
+            return redirect()->route('produtos.index');
+        }else{
+            return redirect()->route('produtos.create');
+        }
     }
 
     /**
@@ -48,8 +84,7 @@ class ProdutoController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
-    {
+    public function show($id) {
         //
     }
 
@@ -59,9 +94,11 @@ class ProdutoController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
-    {
-        //
+    public function edit($id) {
+        $product = $this->product->find($id);
+        $title = 'Editar Produto: '.$product->name;
+        $categorys = ['eletronicos', 'moveis', 'limpeza', 'banho'];
+        return view('painel.products.create-edit', compact('title', 'categorys', 'product'));
     }
 
     /**
@@ -71,9 +108,26 @@ class ProdutoController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
-    {
-        //
+    public function update(ProductFormRequest $request, $id) {
+        
+        //Recupera todos os dados do formulário
+        $dataForm = $request->all();
+        
+        //Recupera o item para editar
+        $product = $this->product->find($id);
+        
+        //Verifica se o produto está ativo
+        $dataForm['active'] = (!isset($dataForm['active']))? 0 : 1;
+        
+        //Altera os itens
+        $update = $product->update($dataForm);
+        
+        //Verifica se realmente editou
+        if($update){
+            return redirect()->route('produtos.index');
+        }else{
+            return redirect()->route('produtos.edit', $id)->with(['errors' => 'Falha ao editar']);
+        }
     }
 
     /**
@@ -82,25 +136,97 @@ class ProdutoController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
-    {
+    public function destroy($id) {
         //
     }
-    
-    public function tests(){
+
+    public function tests() {
+        /*
+          $prod = $this->product;
+          $prod->name = 'Nome do Produto';
+          $prod->number = 123321;
+          $prod->active = true;
+          $prod->category = 'eletronicos';
+          $prod->description = 'Descricao do Produto aqui';
+          $insert = $prod->save();
+
+          if($insert){
+          return 'Inserido com sucesso';
+          }else{
+          return 'Falha ao inserir';
+          }
+         * 
+
+          $insert = $this->product->create([
+          'name' => 'Nome do Produto 3',
+          'number' => 123321,
+          'active' => false,
+          'category' => 'eletronicos',
+          'description' => 'Descricao do Produto aqui'
+          ]);
+
+          if ($insert) {
+          return 'Inserido com sucesso. ID: '.$insert->id;
+          } else {
+          return 'Falha ao inserir';
+          }
+         * 
+         *
+          $prod = $this->product->find(5);
+          //dd($prod);
+          $prod->name = 'Nome 2';
+          $prod->number = 74125;
+          $prod->active = true;
+          $prod->category = 'eletronicos';
+          $prod->description = 'Descricao';
+
+          $update = $prod->save();
+          if($update){
+          return 'Alterado com sucesso';
+          }else{
+          return 'Falha ao alterar.';
+          } *
+        $prod = $this->product->find(6);
+        $update = $prod->update([
+            //Só é necessário passar o que quiser alterar
+            'name' => 'Update 2',
+            'number' => 123321,
+            'active' => false,
+            'category' => 'eletronicos',
+            'description' => 'Descricao do Produto aqui'
+        ]);
+
+        if ($update) {
+            return 'Alterado com sucesso';
+        } else {
+            return 'Falha ao alterar.';
+        }*
+        $prod = $this->product->where('number', '=', 74125);//where('number', 74125)
+        $update = $prod->update([
+            //Só é necessário passar o que quiser alterar
+            'name' => 'Update 2',
+            'number' => 123321,
+            'active' => false,
+            'category' => 'eletronicos',
+            'description' => 'Descricao do Produto aqui'
+        ]);
+
+        if ($update) {
+            return 'Alterado com sucesso';
+        } else {
+            return 'Falha ao alterar.';
+        }*/
         
-        $prod = $this->product;
-        $prod->name = 'Nome do Produto';
-        $prod->number = 123321;
-        $prod->active = true;
-        $prod->category = 'eletronicos';
-        $prod->description = 'Descricao do Produto aqui';
-        $insert = $prod->save();
+        $prod = $this->product->find(3);
+        //delete = $this->product->where('number', 524)->delete();
+        //$prod = $this->product->destroy([1,2,5]);
+        $delete = $prod->delete();
         
-        if($insert){
-            return 'Inserido com sucesso';
+        if($delete){
+            return 'Deletado com sucesso';
         }else{
-            return 'Falha ao inserir';
+            return 'Erro ao deletar';
         }
     }
+
 }
--- resources/views/painel/products/index.blade.php
+++ resources/views/painel/products/index.blade.php
@@ -1,14 +1,34 @@
-<h1>Listagem dos Produtos</h1>
+@extends('painel.templates.template')
 
-<table border="1">
+@section('content')
+
+<h1 class="title-pg">Listagem dos Produtos</h1>
+
+<a href="{{route('produtos.create')}}" class="btn btn-primary btn-add"> <!--url('/painel/produtos/create')-->
+    <span class="glyphicon glyphicon-plus"></span> Cadastrar
+</a>
+
+<table class="table table-striped">
     <tr>
         <th>Nome</th>
         <th>Descrição</th>
+        <th width='100px'>Ações</th>
     </tr>
     @foreach($products as $product)
     <tr>
         <td>{{$product->name}}</td>
         <td>{{$product->description}}</td>
+        <td>
+            <a href="{{route('produtos.edit', $product->id)}}" class="actions edit">
+                <!--url("/painel/produtos/{$product->id}/edit"-->
+                <span class="glyphicon glyphicon-pencil"></span>
+            </a>
+            <a href="" class="actions delete">
+                <span class="glyphicon glyphicon-trash"></span>
+            </a>
+        </td>
     </tr>
     @endforeach
 </table>
+
+@endsection
